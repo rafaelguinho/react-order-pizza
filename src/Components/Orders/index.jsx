38082b87800d3479c766782ef544ec908beaf5da
rafael.monteiro <rafael.monteiro@segfy.com>
2021-03-15 08:04:48 -0300

    Use state hook

---
+++ src/Components/Orders/index.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+
+function Orders() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("https://localhost:44330/api/orders")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setIsLoaded(true);
+          setItems(result);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+  }, []);
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>
+            {item.Flavor} {item.Size}
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
+
+export default Orders;
